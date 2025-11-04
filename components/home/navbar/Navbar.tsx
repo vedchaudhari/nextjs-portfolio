90bddd3b72f0a2b59ee57ee54d47c38c32943cfa
vedchaudhari <vedc2853@gmail.com>
2025-11-05 03:48:44 +0530

    feat: updated overlay screen for mobile navigation menu responsiveness

--- components/home/navbar/Navbar.tsx
+++ components/home/navbar/Navbar.tsx
@@ -6,9 +6,13 @@ import { BiDownload } from 'react-icons/bi'
 import { FaCode } from 'react-icons/fa'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 
-const Navbar = () => {
+type Props = {
+    openNav: () => void;
+}
+
+const Navbar = ({ openNav }: Props) => {
     const [navBg, setnavBg] = useState(false);
-    
+
     useEffect(() => {
         const handler = () => {
             if (window.scrollY >= 90) setnavBg(true);
@@ -21,14 +25,14 @@ const Navbar = () => {
 
 
     return (
-        <div className={`transition-all ${navBg? "bg-[#0f142ed9] shadow-md": "fixed" } duration-200 h-[12vh] z-10000 fixed w-full`} >
+        <div className={`transition-all ${navBg ? "bg-[#0f142ed9] shadow-md" : "fixed"} duration-200 h-[12vh] z-10000 fixed w-full`} >
             <div className='flex items-center h-full justify-between w-[90%] mx-auto '>
                 {/*LOGO*/}
                 <div className='flex items-center space-x-2'>
                     <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col'>
                         <FaCode className='w-5 h-5 text-black' />
                     </div>
-                    <h1 className="text-xl ">Ved</h1>
+                    <h1 className="text-xl text-white">Ved</h1>
                 </div>
                 {/*navlinks*/}
                 <div className='hidden lg:flex items-center space-x-10'>
@@ -46,12 +50,12 @@ const Navbar = () => {
                 <div className='flex items-center space-x-4'>
                     {/*CV button*/}
                     <button className='px-8 py-3 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2' >
-                        <BiDownload className='w-5 h-5'/>
+                        <BiDownload className='w-5 h-5' />
                         <span>Download CV</span>
                     </button>
 
                     {/*Burger menu*/}
-                    <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+                    <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-white lg:hidden' />
 
                 </div>
             </div>
--- components/home/navbar/ResponsiveNavbar.tsx
+++ components/home/navbar/ResponsiveNavbar.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import MobileNavbar from './MobileNavbar'
 
 const ResponsiveNavbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const openNavHandler = () => setShowNav(true);
+  const closeNavHandler = () => setShowNav(false);
   return (
     <div>
-      <Navbar />
-      <MobileNavbar />
+      <Navbar openNav={openNavHandler}/>
+      <MobileNavbar showNav={showNav} closeNav={closeNavHandler} />
     </div>
   )
 }
